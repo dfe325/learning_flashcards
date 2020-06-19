468e4918b5d916af28e11b28eb5de8afb123530b
dfe325 <daneder@gmail.com>
2020-06-19 08:50:52 -0400

    update

--- learning_flashcards/app.py
+++ learning_flashcards/app.py
@@ -6,7 +6,7 @@ from . models import db
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
+
 
     @app.route('/')
     def learning_flashcards():
@@ -20,6 +20,7 @@ def create_app():
         'port': '5432',
     }
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    db.init_app(app)
 
     return app
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,11 @@
 import pytest
 from learning_flashcards.app import create_app
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def app():
     app = create_app()
     return app
+
+@pytest.fixture
+def db_connect():
+    return
